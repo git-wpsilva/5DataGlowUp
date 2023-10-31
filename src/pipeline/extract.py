f9324997f30d51ab13a905fd6010142d3ee5577f
Wesley Pinheiro da Silva <wpsilva.contato@gmail.com>
2023-10-31 20:10:36 -0300

    update pipeline

---
+++ src/pipeline/extract.py
@@ -0,0 +1,29 @@
+import os # biblioteca para manipular arquivos e pastas
+import glob # Biblioteca para listar arquivos de um diretorio
+
+import pandas as pd
+from typing import List
+
+"""
+Função para extrair os dados de vários arquivos csv
+Extraindo dentro de uma pasta, data/input e retornar uma lista de dataframes
+
+args: input_path: caminho para a pasta com os arquivos csv
+
+return: lista de dataframes
+"""
+
+path = "data/input"
+
+def extract_from_csv(path: str) -> List[pd.DataFrame]:
+    all_files = glob.glob(os.path.join(path, "*.csv"))
+    
+    df_list = []
+    for filename in all_files:
+        df = pd.read_csv(filename, sep=";")
+        df_list.append(df)
+    return df_list
+
+if __name__ == "__main__":
+    df_list = extract_from_csv(path)
+    print(df_list)
---
+++ src/pipeline/main.py
@@ -0,0 +1,4 @@
+from pipeline.extract import extract_from_csv
+
+lista_df = extract_from_csv("data/input")
+print(lista_df)
